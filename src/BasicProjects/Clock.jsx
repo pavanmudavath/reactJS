d8c5f7d91b92c8c3e200b9215e4519e2915f3755
pavanmudavath <mudavathpavankumar822@gmail.com>
2024-06-08 14:07:14 +0530

    clock

--- src/BasicProjects/Clock.jsx
+++ src/BasicProjects/Clock.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Clock = () => {
+    let time=new Date().toLocaleTimeString();
+    const [ctime,setCtime]=useState(time);
+
+    const UpdateTime=()=>{
+     time=new Date().toLocaleTimeString();
+        setCtime(time);
+    }
+    setInterval(UpdateTime,(1000));
   return (
-    <div>Clock</div>
+    <div className='flex justify-center items-center h-screen'>
+    
+    <h1 className='font-bold'>{ctime}</h1>
+    
+    </div>
   )
 }
 
